7ebe6b703680569c777c9cad7f60755e9a0f76e8
Davidemeka1234 <elchapodave@gmail.com>
2021-07-12 15:39:48 -0700

    I SUBMITTED MY RAP

---
+++ eeg323/eeg 323/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace eeg_323
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("mide  john!\remeka");
+            Console.WriteLine("my name!\\is emeka");
+            int score = 10 ;
+            if (score > 50)
+            {
+                Console.WriteLine("greater");
+            }
+            else{
+                Console.WriteLine("lesser");
+            }
+
+        }
+            
+            
+    }
+}
